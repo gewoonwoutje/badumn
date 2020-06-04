eff4766636919767d583ded33d561151ef99f9ee
Wout Elstgeest <w.elstgeest@inepro.com>
2020-06-05 01:28:37 +0200

    Add first version

---
+++ src/badumn.ts
@@ -0,0 +1,5 @@
+type Joke = string;
+
+export function next(): Joke {
+	return "badumn.ts";
+}
--- test.ts
+++ test.ts
@@ -1,12 +1,2 @@
-# badumn
-
-A well-typed joke.
-
-## Example
-
-Deno example:
-
-```ts
 import { next } from "https://raw.githubusercontent.com/gewoonwoutje/badumn/master/src/badumn.ts";
 console.log(next());
-```
